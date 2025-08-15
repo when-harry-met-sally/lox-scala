0f554b09fb64581416f7183ba2652b236b72c332
Miles Moran <milesmoran.mu@gmail.com>
2025-08-15 10:31:10 -0500

    added parser module

--- lox/src/Main.scala
+++ lox/src/Main.scala
@@ -1,19 +1,36 @@
 package lox
 import scanner.Scanner
+import parser.Parser
 import token.Token
 
 object Lox {
-  def scanText(text: String): List[Token] = {
-    val tokens = Scanner.scan(text)
-    tokens.foreach(println)
-    tokens
+  def logText(text: String)= {
+    println("---INPUT TEXT---")
+    println(text)
+    println("")
   }
+
+  def logTokens(tokens: List[Token])= {
+    println("-----TOKENS-----")
+    tokens.foreach(println)
+    println("")
+  }
+
+  def logParsTree(tokens: List[Token])= {
+    println("-----TOKENS-----")
+    tokens.foreach(println)
+    println("")
+  }
+
 
   def main(args: Array[String]): Unit = {
     // Example usage
     val text = "1.23"
-    println(text)
-    println("-----")
-    scanText(text)
+    logText(text)
+
+    val tokens = Scanner.scan(text)
+    logTokens(tokens)
+
+    val parsed = Parser.parse(tokens)
   }
 }
---
+++ lox/src/Parser.scala
@@ -0,0 +1,8 @@
+package parser
+import token.{Token,TokenType,Keywords}
+
+object Parser {
+  def parse(tokens: List[Token]) = {
+    println("PARSED") 
+  }
+}
